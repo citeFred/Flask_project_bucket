8edba2447ec703d58af148de12a7202fdc2d8ee7
INYONG KIM <ohnyong87@gmail.com>
2023-07-15 14:35:09 +0900

    Back2_Commit 1
    데이터 명세
    *DB : MongoDB
    *Collection : bucket
    *Document :
    -버킷리스트 메모 내용 : 'bucket':'bucket_receive' / from Frontend #bucket-> formData 'bucket_give':'value'
    
    DB 연결
    *pymongo 임포트 및 client atlas cloud 링크 설정
    *certifi 임포트
    
    app.py DB Insert(Create) 작성
    *@app.route("/bucket", methods=["POST"]) 작성
    *request.form을 통해 ['key']에 해당되는 value를 가져오고 각 변수 "bucket_receive"라는 변수에 담는다
    *doc이라는 객체 생성
    -'bucket' : bucket_receive
    *위 객체는 bucket라는 collection에 insert_one하는 부분을 작성
    *이후, 확인용으로 {'msg' : 'POST 연결 완료! + DB 저장(insert) 완료!'} 라 하드코딩된 key, value가
    *jsonify()에 의해 json 형식의 데이터로 변환(주어진 값과 대응하는 JSON 문자열) 후 반환하고 js로 이동

--- app.py
+++ app.py
@@ -6,18 +6,39 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+# [POST-3] MongoDB사용을 위한 pymongo와 certifi 임포트
+from pymongo import MongoClient
+import certifi
+# [POST-4] DB 커넥션 구성
+ca = certifi.where()
+client = MongoClient('mongodb+srv://ohnyong:test@cluster0.lu7mz8j.mongodb.net/?retryWrites=true&w=majority',tlsCAFile=ca)
+db = client.dbsparta
+
 # "localhost:5001/" URL요청에 메인 뷰 페이지 반환 응답
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# [POST-0] CREATE 부분부터 코드를 작성하는 것(==POST)이 확인이 가능(READ부터하면 데이터가없어서 테스트 어려움)
 # fetch('URL')부분, 반환값은 res로 전달.
 # "localhost:5001/bucket" URL POST방식 요청에 응답
 @app.route("/bucket", methods=["POST"])
 def bucket_post():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'POST 연결 완료!'})
+    # [POST-1] 프론트로부터 무엇을 받아야 하는가? -> 프론트 input으로부터 (html)bucket->(js)bucket_give를 받을 것이다.
+    bucket_receive = request.form['bucket_give']
+    print(bucket_receive)
+    # [POST-2] 클라이언트로부터 받은 데이터를 DB에 넣자 MongoDB연결을 위한 임포트부터 시작(더블체크)
+    # [POST-5] DB연결이 완료되었으니 Dictionary key:value형태 데이터들을 doc=리스트 객체에 담는다.
+    # INSERT_ONE
+    # 저장 - 예시
+    # doc = {'name':'bobby','age':21}
+    doc = {
+        'bucket' : bucket_receive
+    }
+    # [POST-6] doc에 담았으니 DB에 insert 한다.
+    db.bucket.insert_one(doc)
+    # [POST-7] insert가 완료되었으니 완료 메시지를 반환한다.
+    return jsonify({'msg': 'POST 연결 완료!'+'DB 저장 완료!'})
     
 # fetch('URL')부분, 반환값은 res로 전달.
 # "localhost:5001/bucket" URL GET방식 요청에 응답
