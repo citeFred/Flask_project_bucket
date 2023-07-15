3e69e05c0823944e14d917194d01e311960558f8
INYONG KIM <ohnyong87@gmail.com>
2023-07-15 16:00:14 +0900

    Back2_Commit 3
    데이터 명세
    *DB : MongoDB
    *Collection : bucket
    *Document :
    -버킷리스트 메모 내용 : 'bucket':'bucket_receive' / from Frontend #bucket-> formData 'bucket_give':'value'
    
    서버 연결(POST 기록하기 기능 구현 시 작성 완료, 중복부분)
    *flask 서버 생성
    *pymongo 임포트 및 client atlas cloud 링크 설정
    *certifi 임포트
    
    DB find(Read) 작성
    @app.route("/bucket", methods=["GET"]) 작성
    *app.py에서 '/bucket' URL의 GET 방식 요청에 대한 메서드로 접근
    *bucket_get()라 선언된 함수를 실행
    *pymongo의 find()를 통해 bucket라는 collection의 데이터 document 를 모두 찾아냄(읽기)
    *list() 클래스를 통해 리스트 객체로 변환
    *리스트 객체를 all_bucket라는 변수에 담음
    *all_bucket 객체를 jsonify()에 의해 json 형식의 데이터로 변환 후 'result'란 key에 넣어 반환
    
     JavaScript의 show_bucket()라는 함수 작성
    *URL 요청으로 View 페이지가 반환(load)되면
    *JavaScript의 (document).ready() 가 직후에 즉시 실행
    *show_bucket() 함수 실행
    *show_bucket() 내부 fetch()를 통해 '/bucket' URL에 대한 GET 방식 요청 app.py로 이동
    *JavaScript로 다시 돌아와서 첫번째 then()으로 들어간 res는 *response.json()에 의해 Promise 객체로 변환
    *해당 객체 데이터는 data라는 변수에 담기고
    *리스트 형태의 data를 rows 변수에 담고
    *($('#bucket-list').empty(); 로 하드코딩된 테이블 부분을 비워줌)
    *'bucket' key의 value값들이 bucket 변수에 담김
    *temp_html에 위 변수 내용들을 ${bucket} 처럼 출력하는 li>h2 태그 작성
    *index.html에 위 반복문의 결과인 temp_html이 #bucket-list라는 div에 반복문 결과인 li>h2가 append 됨
    
    (결과 View페이지에서는 DB에 저장된 데이터들이 웹 페이지 하단에 목록 형태로 나타남)

--- static/js/script.js
+++ static/js/script.js
@@ -4,8 +4,32 @@ $(document).ready(function () {
 });
 function show_bucket() {
     fetch('/bucket').then(res => res.json()).then(data => {
-        console.log(data)
-        alert(data["msg"]);
+        // json 형식으로 변환, 반환된 데이터가 res 인자로 들어옴
+        // res.json()에 의해 Promise 객체로 변환되어 data에 저장
+        // data 내용 테스트
+        console.log("data===>"+data)
+
+        // data의 내용이 OpenAPI로부터 데이터 받는것과 동일
+        // 리스트 형태의 data를 rows 변수에 담고
+        let rows = data['result']
+        console.log("rows===>"+rows)
+
+        // 반복문 전에 하드코딩 부분 비워주기
+        $('#bucket-list').empty();
+
+        // forEach 반복문을 통해
+        rows.forEach((a)=>{
+            // 리스트에 있는 key의 value들을 각 변수에 담기
+            let bucket = a['bucket']
+            console.log("bucket===>"+rows)
+
+            // index.html에 위 변수들이 들어가도록 백틱 내 자리표시자${variable} 작성한 내용을 temp_html에 작성
+            let temp_html = `<li>
+                                <h2>✅ ${bucket}</h2>
+                            </li>`
+            // 위 temp_html을 index.html의 #cards-box div에 붙여주기.
+            $('#bucket-list').append(temp_html)
+        })
     })
 }
 
