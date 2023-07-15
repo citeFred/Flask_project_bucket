471f6569b1a2542c0e38dd9d61ddf48db3f540bb
INYONG KIM <ohnyong87@gmail.com>
2023-07-15 14:35:23 +0900

    Back2_Commit 2
    데이터 명세
    *DB : MongoDB
    *Collection : bucket
    *Document :
    -버킷리스트 메모 내용 : 'bucket':'bucket_receive' / from Frontend #bucket-> formData 'bucket_give':'value'
    
    JavaScript의 save_bucket()라는 함수 작성
    *index.html에서 값이 입력 된 input으로부터 .val()을 통해 각 id에 해당되는 부분의 value를 얻어 bucket 변수에 저장
    *formData라는 객체를 생성
    *{'bucket_give':bucket}, { ... }, { ... } 라는 Dictionary 형식 데이터를 .append()를 통해  formData에 담기
    *fetch()를 통해 '/bucket' URL에 대한 POST 방식 요청 (+ 위 formData 객체를 body로 추가하여 요청함)
    *.then()절로 app.py의 POST 부분의 jsonify 변환 객체가 반환된 이후
    *반환 데이터는 첫번째 then()절의 res 인자값으로 들어감
    *첫번째 then()으로 들어간 res는 *response.json()에 의해 Promise 객체로 변환
    *해당 객체 데이터는 data라는 변수에 담기고

--- static/js/script.js
+++ static/js/script.js
@@ -11,11 +11,18 @@ function show_bucket() {
 
 // [Create]
 function save_bucket() {
-    let formData = new FormData();
-    formData.append("sample_give", "샘플데이터");
+    // index.html로부터 값 가져오기
+    let bucket = $('#bucket').val()
 
+    // formData 객체를 생성하고
+    let formData = new FormData();
+    // append()통해 {key,value}를 객체에 담는다
+    formData.append("bucket_give", bucket);
+
+    // POST 요청에 위 formData를 body에 담아 요청한다.
     fetch('/bucket', { method: "POST", body: formData, }).then((response) => response.json()).then((data) => {
         alert(data["msg"]);
+        // 브라우저 새로고침 추가
         window.location.reload();
     });
 }
